8f213b7f76d7906ccd4a3a7f7f1617f66e8379af
sabilahj27 <129810344+sabilahj27@users.noreply.github.com>
2025-07-31 14:20:01 +0700

    feat: Implement GiveFeedbackHandler for processing course feedback submissions

---
+++ srv/src/main/java/com/course/cap/course_manage/handler/GiveFeedbackHandler.java
@@ -0,0 +1,43 @@
+package com.course.cap.course_manage.handler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.sap.cds.ql.Insert;
+import com.sap.cds.ql.cqn.CqnInsert;
+import com.sap.cds.services.handler.EventHandler;
+import com.sap.cds.services.handler.annotations.On;
+import com.sap.cds.services.handler.annotations.ServiceName;
+import com.sap.cds.services.persistence.PersistenceService;
+
+import cds.gen.mainservice.CourseFeedbacks;
+import cds.gen.mainservice.CourseFeedbacks_;
+import cds.gen.mainservice.Enrollments;
+import cds.gen.mainservice.EnrollmentsGiveFeedbackContext;
+import cds.gen.mainservice.Enrollments_;
+
+@Component
+@ServiceName("MainService")
+public class GiveFeedbackHandler implements EventHandler {
+    @Autowired
+    private PersistenceService db;
+
+    @On(event = EnrollmentsGiveFeedbackContext.CDS_NAME, entity = Enrollments_.CDS_NAME)
+    public void onGiveFeedback(EnrollmentsGiveFeedbackContext context) {
+        Enrollments enrollment = db.run(context.getCqn()).single(Enrollments.class);
+        CourseFeedbacks feedback = CourseFeedbacks.create();
+
+        String comments = context.getComments();
+        // enrollment.setComments(comments);
+
+        feedback.setComments(comments);
+        feedback.setCourse(feedback.getCourse());
+        feedback.setCreatedAt(null);
+
+        CqnInsert insert = Insert.into(CourseFeedbacks_.CDS_NAME).entry(feedback);
+        db.run(insert);
+
+        context.setCompleted();
+
+    }
+}
